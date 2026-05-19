db72f8c21053c724c893ac530c168e15988102ed
Геор Лолаев <54153406+theJosephGoodman@users.noreply.github.com>
2026-05-19 23:25:21 +0300

    Add: valid-palindrome-ii - submission-13

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-13.py
@@ -0,0 +1,19 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        l,r = 0, len(s)-1
+        
+        while l < r:
+            if s[l] != s[r]:
+                return self.val_pal(s[l:r]) or   self.val_pal(s[l+1:r+1])
+            l= l+1
+            r= r-1
+        return True
+    def val_pal(self, s):
+        l, r = 0, len(s)-1
+
+        while l<r:
+            if s[l] != s[r]:
+                return False
+            l = l+1
+            r = r-1
+        return True
